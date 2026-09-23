c1922f6a4fdc29fe92f1bd3ceb8c8b4e62efa516
Zheng-Bote <zheng.bote@googlemail.com>
2026-09-23 09:00:22 +0200

    v0.16.0

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ MITM_VERSION=$(git describe --tags)
 
 CGO_ENABLED=0 go build -ldflags="-s -w -X main.version=${MITM_VERSION}" -o ./bin/mitm-collector-pg main.go
 
-cp bin/mitm-collector-pg ../../scheduler/mitm_scheduler/bin/.
+cp bin/mitm-collector-pg ../../app/bin/.
